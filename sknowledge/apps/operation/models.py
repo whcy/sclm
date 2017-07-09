9d6a67b3fc2e1f4bbfe39c849db297fe08327219
dvstrom <dvstrom@gmail.com>
2017-07-09 17:07:10 +0800

    和好的

---
+++ sknowledge/apps/operation/models.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+class SclMUser(models.Model):
+    open_id = models.CharField(max_length=50, blank=False, verbose_name='OpenId')
+    user_phone = models.CharField(max_length=20, blank=False, verbose_name=u'手机号')
+    customerclass = models.CharField(max_length=20, blank=False, verbose_name=u'入口平台')
+
+    def __unicode__(self):
+        return self.user_phone
---
+++ sknowledge/apps/operation/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    # url(r'^index2/$', views.index2, name='index2'),
+    url(r'^login/$', views.user_login, name='login'),
+    url(r'^(?P<folder_id>\d+)/$',views.directory_listing,name='directory_listing'),
+    # url(r'^(?P<folder_name>[\w\-]+)/$',views.directory_listing,name='directory_listing'),
+    url(r'^index/$', views.index, name='index'),
+    url(r'^phone_bind/$', views.phone_bind, name='phone_bind'),
+    url(r'^send_msg/$', views.send_msg, name='send_msg')
+
+]
